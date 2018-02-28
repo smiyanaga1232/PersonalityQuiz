bc79b1de9f3a7efe7d0d2580e04d3d0408e03c64
Shin Miyanaga <smiyanaga1232@jh-mba158-16.d214.org>
2018-02-28 11:21:28 -0600

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Shin Miyanaga on 2/28/18.
+//  Copyright © 2018 JohnHerseyHighschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
