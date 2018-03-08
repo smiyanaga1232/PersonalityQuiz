f8a142dde8bba49a028b2358a1acb19519695593
Shin Miyanaga <smiyanaga1232@jh-mba158-16.d214.org>
2018-03-08 11:30:46 -0600

    Linking stack view to UIOutlets

--- PersonalityQuiz/QuestionsViewController.swift
+++ PersonalityQuiz/QuestionsViewController.swift
@@ -9,27 +9,55 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var singleStackView: UIStackView!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    
+    @IBOutlet weak var rangeStackView: UIStackView!
+    @IBOutlet weak var progressView: UIProgressView!
+  
+    var questionIndex = 0
+    var questions : [Question] = [
+        Question(text: "Which food do you like the most?" ,
+        type: .single,
+        answers: [
+        Answer(text: "Steak", type: .dog),
+        Answer(text: "Fish", type: .cat),
+        Answer(text: "Carrots", type: .rabbit),
+        Answer(text: "Corn", type: .turtle)
+        ]),
+        Question(text: "Which activities do you enjoy?" ,
+                 type: .multiple,
+                 answers: [
+                    Answer(text: "Swimming", type: .turtle),
+                    Answer(text: "Sleeping", type: .cat),
+                    Answer(text: "Cuddling", type: .rabbit),
+                    Answer(text: "Eating", type: .dog)
+            ]),
+        Question(text: "How much do you enjoy car rides?",
+                 type: .ranged,
+                 answers: [
+                    Answer(text: "I dislike them", type: .cat),
+                    Answer(text: "I get alittle nervous", type: .rabbit),
+                    Answer(text: "I barely notice them", type: .turtle),
+                    Answer(text: "I love them", type: .dog)
+                    ])
+        ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
